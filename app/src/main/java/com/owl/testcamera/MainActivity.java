a911fda093f5c4eedbf11b683b078fe3c67896f5
Qiushunming <qiu592198740@gmail.com>
2017-08-07 17:42:31 +0800

    add log class.

--- app/src/main/java/com/owl/testcamera/MainActivity.java
+++ app/src/main/java/com/owl/testcamera/MainActivity.java
@@ -2,15 +2,15 @@ package com.owl.testcamera;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+
+import com.owl.utils.MLog;
 
 public class MainActivity extends Activity {
-    private final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG,"onCreate");
+        MLog.d("onCreate");
     }
 }
---
+++ recordlib/src/main/java/com/owl/utils/MLog.java
@@ -0,0 +1,192 @@
+package com.owl.utils;
+
+import android.text.TextUtils;
+import android.util.Log;
+
+/**
+ * Created by qiushunming on 17/8/5.
+ */
+
+public class MLog {
+    public static String customTagPrefix = "";
+
+    private static boolean IS_DEBUG = true;
+
+    public static void setDebug(boolean debug) {
+        IS_DEBUG = debug;
+    }
+
+    private static String generateTag(StackTraceElement caller) {
+        String tag = "%s.%s(L:%d)";
+        String callerClazzName = caller.getClassName();
+        callerClazzName = callerClazzName.substring(callerClazzName
+                .lastIndexOf(".") + 1);
+        tag = String.format(tag, callerClazzName, caller.getMethodName(),
+                caller.getLineNumber());
+        tag = TextUtils.isEmpty(customTagPrefix) ? tag : customTagPrefix + ":"
+                + tag;
+        return tag;
+    }
+
+    public static CustomLogger customLogger;
+
+    public interface CustomLogger {
+        void d(String tag, String content);
+
+        void d(String tag, String content, Throwable tr);
+
+        void w(String tag, String content);
+
+        void w(String tag, String content, Throwable tr);
+
+        void e(String tag, String content);
+
+        void e(String tag, String content, Throwable tr);
+
+        void i(String tag, String content);
+
+        void i(String tag, String content, Throwable tr);
+
+        void v(String tag, String content);
+
+        void v(String tag, String content, Throwable tr);
+
+    }
+
+    public static void v(String content) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.v(tag, content);
+        } else {
+            Log.v(tag, content);
+        }
+
+
+    }
+
+    public static void v(String content, Throwable tr) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.v(tag, content, tr);
+        } else {
+            Log.v(tag, content, tr);
+        }
+    }
+
+    public static void d(String content) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.d(tag, content);
+        } else {
+            Log.d(tag, content);
+        }
+    }
+
+    public static void d(String content, Throwable tr) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.d(tag, content, tr);
+        } else {
+            Log.d(tag, content, tr);
+        }
+    }
+
+    public static void w(String content) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.w(tag, content);
+        } else {
+            Log.w(tag, content);
+        }
+    }
+
+    public static void w(String content, Throwable tr) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.w(tag, content, tr);
+        } else {
+            Log.w(tag, content, tr);
+        }
+    }
+
+    public static void e(String content) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.e(tag, content);
+        } else {
+            Log.e(tag, content);
+        }
+    }
+
+    public static void e(String content, Throwable tr) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.e(tag, content, tr);
+        } else {
+            Log.e(tag, content, tr);
+        }
+    }
+
+    public static void i(String content) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.i(tag, content);
+        } else {
+            Log.i(tag, content);
+        }
+    }
+
+    public static void i(String content, Throwable tr) {
+        if (!IS_DEBUG)
+            return;
+        StackTraceElement caller = getCallerStackTraceElement();
+        String tag = generateTag(caller);
+
+        if (customLogger != null) {
+            customLogger.i(tag, content, tr);
+        } else {
+            Log.i(tag, content, tr);
+        }
+    }
+
+    public static StackTraceElement getCallerStackTraceElement() {
+        return Thread.currentThread().getStackTrace()[4];
+    }
+
+}
